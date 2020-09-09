bace800b0c5e7a79fe43a346835b4893deca7f18
Jørgen I Christensen <jichristensen@hotmail.com>
2020-09-09 13:01:12 +0200

    ToArabic backend

--- RomanI/Controllers/ToArabicController.cs
+++ RomanI/Controllers/ToArabicController.cs
@@ -14,9 +14,45 @@ namespace RomanI.Controllers
         [HttpGet]
         public int Get(string roman)
         {
-            if (roman == "I") return 1;
-            if (roman == "IV") return 4;
-            else return 0;
+            InitDictionary();
+            int arabicNumber = 0;
+
+            while (roman.Length != 0)
+            {
+                foreach (string token in _romanToArabic.Keys)
+                {
+                    if (roman.StartsWith(token))
+                    {
+                        roman = roman.Substring(token.Length);
+                        arabicNumber += _romanToArabic[token];
+                    }
+                }
+            }
+
+            return arabicNumber;
         }
+
+        private Dictionary<string, int> _romanToArabic;
+        private void InitDictionary()
+        {
+            _romanToArabic = new Dictionary<string, int>
+            {
+                {"M", 1000},
+                {"CM", 900},
+                {"D",  500},
+                {"CD", 400},
+                {"C",  100},
+                {"XC",  90},
+                {"L",   50},
+                {"XL",  40},
+                {"X",   10},
+                {"IX",   9},
+                {"V",    5},
+                {"IV",   4},
+                {"I",    1}
+            };
+        }
+
+
     }
 }
